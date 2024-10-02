8ade31f34d0547d14814b8b218df8cd5cfefe56f
realtimshady16 <166579177+realtimshady16@users.noreply.github.com>
2024-10-02 12:27:44 +0200

    Add files via upload

--- Shapemaker.java
+++ Shapemaker.java
@@ -6,12 +6,18 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Shapemaker extends JFrame{
-    protected class ShapeInfo{
+    protected static class ShapeInfo{
         private String shape;
         private String colour;
         private String fillColour;
-        private String[] dimensions = new String[3];
+        private String[] dimensions;
 
+        public ShapeInfo(){
+            this.shape = "circle";
+            this.colour = "red";
+            this.fillColour = "no";
+            this.dimensions = new String[3];
+        }
         public String getShape(){
             return shape;
         }
@@ -40,9 +46,17 @@ public class Shapemaker extends JFrame{
     }
     public Shapemaker(String title){
         setTitle(title);
+        JButton drawButton = new JButton("Draw!");
+        JPanel mainPanel = new JPanel();
+        //drawButton.setSize()
+        DrawPanel drawPanel;
         ShapeInfo shapeInfo = new ShapeInfo();
         InfoPanel infoPanel = new InfoPanel(shapeInfo);
-        DrawPanel drawPanel = new DrawPanel(shapeInfo); //the drawPanel has information that infoPanel needs to get and return to be passed into the drawPanel. how do I structure this code so that the user can interact with infoPanel and said user-info can be passed into DrawPanel
+        //mainPanel.setLayout(new GridLayout(0, 1));
+        //mainPanel.add(drawButton);
+        drawPanel = new DrawPanel(shapeInfo); 
+        //mainPanel.add(drawPanel);
+        //drawPanel.setShapeInfo(shapeInfo);
         setLayout(new BorderLayout());
         add(drawPanel, BorderLayout.CENTER);
         add(infoPanel, BorderLayout.EAST);
@@ -54,7 +68,7 @@ public class Shapemaker extends JFrame{
         maker.setVisible(true);
     }
 
-    protected class InfoPanel extends JPanel{
+    protected static class InfoPanel extends JPanel{
         private String shape;
         private String colour;
         private String fillColour;
@@ -79,10 +93,10 @@ public class Shapemaker extends JFrame{
             add(dimensionsPanel);
             setSize(200, 600);
             setVisible(true);
-            shapeInfo.setShape(getShape());
-            shapeInfo.setColour(getColour());
-            shapeInfo.setFillColour(getFillColour());
-            shapeInfo.setDimensions(getDimensions());
+            shapeInfo.setShape(shape);//getShape());
+            shapeInfo.setColour(colour);//getColour());
+            shapeInfo.setFillColour(fillColour);//getFillColour());
+            shapeInfo.setDimensions(dimensions);//getDimensions());
 
         }
         public JPanel createShapePanel(){
@@ -147,8 +161,8 @@ public class Shapemaker extends JFrame{
             group.add(no);
             setFill.addActionListener(new ActionListener(){
                 public void actionPerformed(ActionEvent e){
-                    if(yes.isSelected()) colour = "yes";
-                    else if(no.isSelected()) colour = "no";
+                    if(yes.isSelected()) colour="true";
+                    else if(no.isSelected()) colour="false";
                 }
             });
             fillColourPanel.setLayout(new FlowLayout());
@@ -196,27 +210,129 @@ public class Shapemaker extends JFrame{
             return dimensions;
         }
     }
-    protected class DrawPanel extends JPanel{
+    protected static class DrawPanel extends JPanel{
         private ShapeInfo shapeInfo;
-        private JButton drawButton;
-        public DrawPanel(ShapeInfo si){
-            shapeInfo = si;
+        private JButton drawButton = new JButton("Draw!");
+        public DrawPanel(ShapeInfo shapeInfo){
             Border border = BorderFactory.createLineBorder(Color.BLACK, 2);
             setBorder(border);
+            setLayout(new BorderLayout());
+            drawButton.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent e){
+                    setShapeInfo(shapeInfo);
+                    repaint();
+                    revalidate();
+                }
+            });
             add(drawButton, BorderLayout.SOUTH);
             setPreferredSize(new Dimension(600, 600));
+            setEnabled(true);
             setVisible(true);
         }
-        public void paintRectangle(Graphics g){
-
-        }
-        public void paintCircle(Graphics g){
-
-        }
-        public void paintTriangle(Graphics g){
+        @Override
+        protected void paintComponent(Graphics g){
             super.paintComponent(g);
+            Graphics2D g2D = (Graphics2D) g;
+            if(shapeInfo != null){
+                switch(shapeInfo.getShape().toLowerCase()){
+                    case "Rectangle": paintRectangle(g2D); break;
+                    case "Triangle": paintTriangle(g2D); break;
+                    case "Circle": paintCircle(g2D); break;
+                }
+            }
+        }
+        private Color getColorFromString(String colorStr) {
+            switch (colorStr.toLowerCase()) {
+                case "red":return Color.RED;
+                case "green":return Color.GREEN;
+                case "blue":return Color.BLUE;
+                case "black":return Color.BLACK;
+                default:return Color.BLACK;
+            }
+        }
+        public void paintRectangle(Graphics2D g2D){
+            String[] dimensions = shapeInfo.getDimensions();
+            Double rwidth = Double.parseDouble(dimensions[0]);
+            Double rlength = Double.parseDouble(dimensions[1]);
+            String colour = shapeInfo.getColour();
+            Boolean fillColour = Boolean.parseBoolean(shapeInfo.getFillColour());
+            Rectangle r = new Rectangle(rwidth, rlength, colour, fillColour);
+            int width = getWidth();
+            int height = getHeight();
+            int rectX = 300;//width/2 - (int) r.getWidth()/2;
+            int rectY = 500;//height/2 - (int) r.getHeight()/2;
+            g2D.setColor(getColorFromString(colour));
+            g2D.fillRect(rectX, rectY, (int) r.getWidth(), (int) r.getHeight());
+            repaint();
+            
+        }
+        public void paintCircle(Graphics2D g2D){
+            String[] dimensions = shapeInfo.getDimensions();
+            Double radius = Double.parseDouble(dimensions[0]);
+            String colour = shapeInfo.getColour();
+            Boolean fillColour = Boolean.parseBoolean(shapeInfo.getFillColour());
+            Circle c = new Circle(radius, colour, fillColour);
+            int width = getWidth();
+            int height = getHeight();
+            int circX = 150;//width/2 - (int) c.getRadius()/2;
+            int circY = 150;//height/2 - (int) c.getRadius()/2;
+            g2D.setColor(getColorFromString(colour));
+            g2D.fillOval(circX, circY, (int) c.getRadius(), (int) c.getRadius());
+            repaint();
 
+        }
+        public void paintTriangle(Graphics2D g2D){
+            String[] dimensions = shapeInfo.getDimensions();
+            Double side1 = Double.parseDouble(dimensions[0]);
+            Double side2 = Double.parseDouble(dimensions[1]);
+            Double side3 = Double.parseDouble(dimensions[2]);
+            String colour = shapeInfo.getColour();
+            Boolean fillColour = Boolean.parseBoolean(shapeInfo.getFillColour());
+            Triangle t = new Triangle(side1, side2, side3, colour, fillColour);
+            double angleA = Math.asin(2*t.getArea()/(t.getSide2()*t.getSide3()));
+            double angleB = Math.asin(2*t.getArea()/(t.getSide1()*t.getSide3()));
+            double angleC = Math.asin(2*t.getArea()/(t.getSide1()*t.getSide2()));
+            int width = getWidth();
+            int height = getHeight();
+            double x1 = width/2;
+            double y1 = height/2;
+            double x2 = x1 + t.getSide2() * Math.cos(angleA);
+            double y2 = y1 + t.getSide2() * Math.sin(angleA);
+            double x3 = x1 + t.getSide3();
+            double y3 = y1;
+            int[] xPoints = {150, 250, 350};//{(int)x1, (int)x2, (int)x3};
+            int[] yPoints = {150, 250, 350};//{(int)y1, (int)y2, (int)y3};
+            g2D.setColor(getColorFromString(colour));
+            g2D.fillPolygon(xPoints, yPoints, 3);
+            repaint();
+            
+        }
+        public void setShapeInfo(ShapeInfo si){
+            shapeInfo = si;
         }
     }
+    // double angleA = Math.asin(2*t.getArea()/(t.getSide2()*t.getSide3()));
+    // double angleB = Math.asin(2*t.getArea()/(t.getSide1()*t.getSide3()));
+    // double angleC = Math.asin(2*t.getArea()/(t.getSide1()*t.getSide2()));
+    // int width = getWidth();
+    // int height = getHeight();
+    // int rectX = width/2 - (int) r.getWidth()/2;
+    // int rectY = height/2 - (int) r.getHeight()/2;
+    // int circX = width/2 - (int) c.getRadius()/2;
+    // int circY = height/2 - (int) c.getRadius()/2;
+    // double x1 = width/2;
+    // double y1 = height/2;
+    // double x2 = x1 + t.getSide2() * Math.cos(angleA);
+    // double y2 = y1 + t.getSide2() * Math.sin(angleA);
+    // double x3 = x1 + t.getSide3();
+    // double y3 = y1;
+    // int[] xPoints = {(int)x1, (int)x2, (int)x3};
+    // int[] yPoints = {(int)y1, (int)y2, (int)y3};
+    // g2D.setColor(Color.getColor(r.getColor()));
+    // g2D.fillRect(rectX, rectY, (int) r.getWidth(), (int) r.getHeight());
+    // g2D.setColor(Color.getColor(c.getColor()));
+    // g2D.fillOval(circX, circY, (int) c.getRadius(), (int) c.getRadius());
+    // g2D.setColor(Color.getColor(t.getColor()));
+    // g2D.fillPolygon(xPoints, yPoints, 3);
 
 }
